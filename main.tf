b1b100ac31cdc7b7268182cd1ea782455da742db
manbeer1999 <mnrewari2017@gmail.com>
2025-08-28 11:25:31 -0700

    Finalize CSPM: tfsec workflow with JSON+Pages, kube-bench dummy, Gatekeeper, dashboard

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -11,4 +21,5 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
+
 
